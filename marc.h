212f14c9a4a010e39c928676cc741db2aadc2ca5
Cliff Snyder <cliff.snyder@gmail.com>
2020-10-03 11:23:07 -0400

    doc comments

--- marc.h
+++ marc.h
@@ -24,12 +24,27 @@ typedef struct marcrec {
 /**
  * @brief read a marcrec from a file
  *
- * @param rec a pointer to an allocated marcrec object
+ * if rec->fields is non-null this function will assume you want fields processed;
+ * otherwise it will only read the raw record
+ *
+ * @param rec a pointer to an allocated marcrec object to be populated
  * @param buf a pointer to an allocated buffer to read into
  * @param in a pointer to an open FILE to read from
  * @return int the number of bytes read (-1 if the number of bytes was fewer than expected)
  */
 int marcrec_read(marcrec *rec, char *buf, FILE *in);
+
+/**
+ * @brief read a marcrec from a buffer
+ *
+ * if rec->fields is non-null this function will assume you want fields processed;
+ * otherwise it will only read the raw record
+ *
+ * @param rec a pointer to an allocated marcrec object to be populated
+ * @param buf a pointer to a buffer containing a marc record
+ * @param len the number of bytes in the marc record, or 0 if the length is unknown and should be computed
+ * @return int the number of bytes processed
+ */
 int marcrec_from_buffer(marcrec *rec, char *buf, int len);
 
 void marcrec_dump(const marcrec *rec, FILE *out);
