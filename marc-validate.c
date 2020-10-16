38ef2e851edcc5f9658b2f31eb6668b96a2c0dc0
Cliff Snyder <cliff.snyder@gmail.com>
2020-10-16 14:55:44 -0400

    compute and output totals

--- marc-validate.c
+++ marc-validate.c
@@ -8,6 +8,9 @@ extern int __marc_main_fieldspec_count;
 extern int __marc_main_infile_count;
 extern const char **__marc_main_infiles;
 
+int __marc_validate_valid_count = 0;
+int __marc_validate_total_count = 0;
+
 void print_result(FILE *out, marcrec *rec, int current_file, gzFile in)
 {
     if(__marc_main_fieldspec_count != 0)
@@ -20,5 +23,9 @@ void print_result(FILE *out, marcrec *rec, int current_file, gzFile in)
         if (marcrec_validate(rec) == 0)
             valid++;
     }
-    fprintf(out, "%s: %i of %i valid\n", __marc_main_infiles[current_file], valid, count);
+    __marc_validate_valid_count += valid;
+    __marc_validate_total_count += count;
+    fprintf(out, "%s: %i of %i valid (total: %i/%i)\n",
+        __marc_main_infiles[current_file], valid, count,
+        __marc_validate_valid_count, __marc_validate_total_count);
 }
