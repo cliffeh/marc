b9be61660ac88c9266edf6e914605fad221e8f88
Cliff Snyder <cliff.snyder@gmail.com>
2020-10-18 21:31:21 -0400

    comment

--- util.h
+++ util.h
@@ -1,6 +1,13 @@
 #ifndef __MARC_UTIL_H
 #define __MARC_UTIL_H 1
 
+/**
+ * @brief convert a substring into an integer
+ *
+ * @param p pointer to the beginning of the substring
+ * @param n number of characters to convert
+ * @return int the number represented by the substring
+ */
 int atoin(const char *p, int n);
 
 #endif
