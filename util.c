a434834e5881496491a0accc2aa0741d4c727e89
Cliff Snyder <cliff.snyder@gmail.com>
2020-10-20 11:32:34 -0400

    minor change

--- util.c
+++ util.c
@@ -6,9 +6,9 @@ int atoin(const char *p, int n)
     for (int i = 0; i < n; i++)
     {
         r *= 10;
-        if (!isdigit(*(p + i)))
+        if (!isdigit(p[i]))
             return -1;
-        r += (*(p + i) - '0');
+        r += (p[i] - '0');
     }
     return r;
 }
