a967e4a25f4ee574800c5ef563348528709ad7d8
Cliff Snyder <cliff.snyder@gmail.com>
2020-10-20 07:44:01 -0400

    woops! remember to free...

--- test/dynamic-alloc.c
+++ test/dynamic-alloc.c
@@ -3,13 +3,15 @@
 int main()
 {
     marcfile *in = marcfile_open("test/testfile.marc.gz", "r");
+    marcrec *rec;
 
     int count = 0, valid = 0;
-    for (marcrec *rec = marcrec_read(0, in); rec; rec = marcrec_read(0, in))
+    for (rec = marcrec_read(0, in); rec; rec = marcrec_read(0, in))
     {
         if (marcrec_validate(rec) == 0)
             valid++;
         count++;
+        marcrec_free(rec);
     }
     fprintf(stdout, "%i/%i valid\n", valid, count);
 
