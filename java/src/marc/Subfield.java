7410d762bc8a9b5534254eb087b322f32aef4974
Cliff Snyder <cliff.snyder@gmail.com>
2020-10-30 22:40:42 -0400

    refactoring/renaming

---
+++ java/src/marc/Subfield.java
@@ -0,0 +1,23 @@
+package marc;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+public class Subfield {
+    public char code;
+    private ByteBuffer data;
+
+    public Subfield(char code, ByteBuffer data) {
+        this.code = code;
+        this.data = data;
+    }
+
+    public void write(OutputStream out) throws IOException {
+        out.write(code);
+        data.position(0);
+        while (data.hasRemaining()) {
+            out.write(data.get());
+        }
+    }
+}
