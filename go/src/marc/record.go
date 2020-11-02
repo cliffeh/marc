39b750d3c1f4d4d79f9398f5f5655d934dbbd3c8
Cliff Snyder <cliff.snyder@gmail.com>
2020-11-01 19:00:40 -0500

    comments

--- go/src/marc/record.go
+++ go/src/marc/record.go
@@ -1,10 +1,12 @@
 package marc
 
+// Record is the basic representation of a MARC record
 type Record struct {
 	data   []byte
 	fields []Field
 }
 
+// Leader retrieves the leader (first 24 bytes) of a MARC record
 func (rec Record) Leader() []byte {
 	return rec.data[0:24]
 }
