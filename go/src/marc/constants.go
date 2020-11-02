07b9a9173c50d68c244818e235f43125dfe5a99f
Cliff Snyder <cliff.snyder@gmail.com>
2020-11-01 19:54:32 -0500

    constants

---
+++ go/src/marc/constants.go
@@ -0,0 +1,10 @@
+package marc
+
+// RecordTerminator byte terminates a MARC record
+const RecordTerminator = 0x1d
+
+// FieldTerminator byte terminates a MARC field
+const FieldTerminator = 0x1e
+
+// SubfieldDelimiter byte delimits a MARC field subfield
+const SubfieldDelimiter = 0x1f
