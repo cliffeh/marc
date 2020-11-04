0090d2b322ae2181a280e3dc4795bca6983502b5
Cliff Snyder <cliff.snyder@gmail.com>
2020-11-04 11:21:24 -0500

    gzip support

--- go/main.go
+++ go/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"compress/gzip"
+	"io"
 	"os"
 
 	"marc"
@@ -14,9 +16,20 @@ func check(e error) {
 }
 
 func main() {
-	in := marc.NewReader(os.Stdin)
+	br := bufio.NewReader(os.Stdin)
+	testBytes, err := br.Peek(2)
+	check(err)
+	var in io.Reader
+	if testBytes[0] == 31 && testBytes[1] == 139 { // gzip
+		in, err = gzip.NewReader(br)
+		check(err)
+	} else {
+		in = br
+	}
+
+	mr := marc.NewReader(in)
 	out := bufio.NewWriter(os.Stdout)
-	for rec, err := in.ReadRecord(); rec != nil; rec, err = in.ReadRecord() {
+	for rec, err := mr.ReadRecord(); rec != nil; rec, err = mr.ReadRecord() {
 		check(err)
 		rec.Process().Write(out)
 	}
--- go/src/marc/reader.go
+++ go/src/marc/reader.go
@@ -21,7 +21,7 @@ func NewReader(rd io.Reader) *RecordReader {
 func (rr *RecordReader) ReadRecord() (r *Record, e error) {
 	r = new(Record)
 	buf := make([]byte, 24)
-	n, e := rr.rd.Read(buf)
+	n, e := io.ReadFull(rr.rd, buf)
 	if n != 24 {
 		return nil, fmt.Errorf("leader fewer than 24 bytes")
 	}
@@ -29,7 +29,7 @@ func (rr *RecordReader) ReadRecord() (r *Record, e error) {
 	length := atoi(buf[0:5])
 	r.data = make([]byte, length)
 	copy(r.data, buf)
-	n, e = rr.rd.Read(r.data[24:])
+	n, e = io.ReadFull(rr.rd, r.data[24:])
 	if n < length-24 {
 		return nil, fmt.Errorf("too few bytes; expected %d, got %d", length, n+24)
 	}
