4f3d2d4491625552804288b7cdde40559118e4df
Cliff Snyder <cliff.snyder@gmail.com>
2020-11-04 11:39:53 -0500

    group constant declaration

--- go/src/marc/constants.go
+++ go/src/marc/constants.go
@@ -1,10 +1,12 @@
 package marc
 
-// RecordTerminator byte terminates a MARC record
-const RecordTerminator = 0x1d
+const (
+	// RecordTerminator byte terminates a MARC record
+	RecordTerminator = 0x1d
 
-// FieldTerminator byte terminates a MARC field
-const FieldTerminator = 0x1e
+	// FieldTerminator byte terminates a MARC field
+	FieldTerminator = 0x1e
 
-// SubfieldDelimiter byte delimits a MARC field subfield
-const SubfieldDelimiter = 0x1f
+	// SubfieldDelimiter byte delimits a MARC field subfield
+	SubfieldDelimiter = 0x1f
+)
